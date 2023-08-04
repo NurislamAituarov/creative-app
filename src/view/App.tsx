1564c0e9741f21ca58806b655177fe6cefe436ac
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-08-04 12:27:18 +0600

    Created new page creative scroll

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,11 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import LocomotiveScroll from 'locomotive-scroll';
+
 import { FairFores } from '../pages/FairForest';
+import { CreateScroll } from '../pages/CreativeScroll';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const history = useLocation();
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    if (history.pathname === '/') {
+      wrapperRef.current &&
+        new LocomotiveScroll({
+          el: wrapperRef.current,
+          smooth: true,
+          getSpeed: true,
+        });
+    }
+  }, []);
+
   return (
-    <div className="wrapper">
+    <div className="wrapper" data-scroll-container ref={wrapperRef}>
       <Routes>
-        <Route path="/" element={<FairFores />} />
+        <Route path="/fair-forest" element={<FairFores />} />
+        <Route path="/" element={<CreateScroll />} />
       </Routes>
     </div>
   );
